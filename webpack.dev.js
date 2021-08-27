f5d337457806e27cb7283bb3933a2eb17ec36aaf
Artur8800 <artursviridonov@gmail.com>
2021-08-27 22:24:05 +0300

    f

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,8 +3,6 @@ const common = require("./webpack.common.js");
 const webpack = require("webpack");
 const path = require("path");
 
-// const HotModuleReplacementPlugin = require("HotModuleReplacementPlugin");
-
 module.exports = merge(common, {
   mode: "development",
   devtool: "inline-source-map",
@@ -15,8 +13,6 @@ module.exports = merge(common, {
   target: 'web',
   devServer: {
     watchFiles: ['src/*'],
-
-    //contentBase: path.join(__dirname, "dist"),
     port: 9000,
     client: {
       overlay: {
